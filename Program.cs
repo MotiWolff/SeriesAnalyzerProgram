b61dff4895449aebae6ca7e70cd0f54cf310f80f
MotiWolff <motiwolff@gmail.com>
2025-05-12 19:56:53 +0300

    Add Program Setup

--- Program.cs
+++ Program.cs
@@ -1,1 +1,83 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeriesAnalyzerProgram
+{
+    class SeriesAnalyzerProgram
+    {
+        static void Main(string[] args)
+        {
+            // Program manager, handling input, looping over menu
+        }
+
+        // Input & Validation
+        static List<double> ConvertArgsToSeries(string[] args)
+        {
+
+        }
+
+        static bool TryConvertPositiveNumbers(string input, out double number)
+        {
+
+        }
+
+        static List<double> NewInputSeries()
+        {
+
+        }
+
+        // Menu Handling
+        static void DisplayMenu()
+        {
+
+        }
+
+        static char GetMenuOption()
+        {
+
+        }
+
+        static (bool, List<double>) ProcessOption(char option, List<double> series)
+        {
+
+        }
+
+        // Display
+        static void ShowSeries(List<double> series, string label)
+        {
+
+        }
+
+        // Series Operations
+        static List<double> GetReversed(List<double> series)
+        {
+
+        }
+
+        static List<double> GetSorted(List<double> series)
+        {
+
+        }
+
+        static double GetMax(List<double> series)
+        {
+
+        }
+
+        static double GetMin(List<double> series)
+        {
+
+        }
+
+        static double GetAverage(List<double> series)
+        {
+
+        }
+
+        static double GetSum(List<double> series)
+        {
+
+        }
+
+    }
+}
